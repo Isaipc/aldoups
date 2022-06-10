236394c0e49de3b5f07ec64561d5b2da54b44d02
Isai Polito <ispolitoc@gmail.com>
2022-06-10 18:50:32 -0500

    fix: sql instructions

--- db.sql
+++ db.sql
@@ -1,33 +1,41 @@
+DROP IF EXISTS aldoups_proyecto;
+
 CREATE DATABASE aldoups_proyecto;
 
 USE aldoups_proyecto;
 
 CREATE TABLE productos(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(60) NOT NULL,
     precio DOUBLE NOT NULL,
     descripcion TEXT NOT NULL,
     stock INT NOT NULL,
     fecha_ingreso TIMESTAMP
     fecha_modificacion TIMESTAMP
-);
+    
+    PRIMARY KEY (id)
+) ENGINE=INNODB;
 
 CREATE TABLE categorias(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(60) NOT NULL,
     descripcion TEXT,
     fecha_ingreso TIMESTAMP,
     fecha_modificacion TIMESTAMP
-)
+
+    PRIMARY KEY (id)
+) ENGINE=INNODB;
 
 CREATE TABLE usuarios(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(15) NOT NULL,
     password VARCHAR NOT NULL,
     nombre VARCHAR(30) NOT NULL,
     fecha_ingreso TIMESTAMP,
     fecha_modificacion TIMESTAMP
-)
+
+    PRIMARY KEY (id)
+) ENGINE=INNODB;
 
 CREATE TABLE ventas(
     id INT NOT NULL AUTO_INCREMENT,
@@ -39,7 +47,7 @@ CREATE TABLE ventas(
 
     FOREIGN KEY (usuario_id)
         REFERENCES usuarios(id)
-) ENGINET=INNODB;
+) ENGINE=INNODB;
 
 CREATE TABLE producto_vendido(
     id INT AUTO_INCREMENT,

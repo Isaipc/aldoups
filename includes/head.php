2f374978ae5f2c000720c5ac752869137ba0476d
Isai Polito <ispolitoc@gmail.com>
2022-06-11 00:01:00 -0500

    add includes php files

---
+++ includes/head.php
@@ -0,0 +1,3 @@
+    <link rel="stylesheet" href="/css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="/css/app.css">
---
+++ includes/scripts.php
@@ -0,0 +1,3 @@
+<script type="text/javascript" src="/js/popper.min.js"></script>
+<script type="text/javascript" src="/js/bootstrap/bootstrap.min.js"></script>
+<script type="text/javascript" src="/js/main.js"></script>

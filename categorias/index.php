6ac1c4136dfb12fa461a79c97f7fd8baf451a459
Isai Polito <ispolitoc@gmail.com>
2022-06-11 01:03:58 -0500

    update categorias index page

--- categorias/index.php
+++ categorias/index.php
@@ -12,8 +12,31 @@
 <body>
     <?php include '../includes/navbar.php' ?>
     <div class="container p-2">
+        <h2>Categorias</h2>
+        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalIngresar">
+            <i class="bi bi-plus"></i>
+            Nuevo
+        </button>
 
-        <h1>Categorias</h1>
+        <!-- Tabla de visualizacion de datos -->
+        <div class="card">
+            <div class="card-body">
+                <table id="account-grid" class="table table-responsive table-hover">
+                    <thead>
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th>Descripcion</th>
+                        <th>Fecha de ingreso</th>
+                    </thead>
+                    <tbody id="categorias"></tbody>
+                </table>
+                <div id="grid-alert" class="alert alert-warning" role="alert">
+                    <i class="fas fa-info-circle"></i><strong id="grid-message"></strong>
+                </div>
+            </div>
+        </div>
+
+        <!-- Modal ingreso -->
         <div class="modal fade" id="modalIngresar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -23,123 +46,81 @@
                     </div>
                     <div class="modal-body px-5">
                         <!-- creacion de formulario -->
-                        <form id="form" action="#" method="post" onsubmit="return valida(this);">
-                            <div class="row">
+                        <form id="form" action="#" method="POST" onsubmit="return valida(this);">
+                            <div class="row mb-2">
                                 <input type="hidden" name="id" id="id">
-                                <div class="col-lg-6 col-md-6 md-form">
-                                    <input id="name" name="nombre" type="text" class="text-uppercase form-control">
-                                    <label for="name">nombre</label>
-                                </div>
-                                <div class="col-lg-6 col-md-6 md-form">
-                                    <input id="cost" name="precio" type="text" class="text-uppercase form-control">
-                                    <label for="cost">descripcion<label>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                                    <button class="btn btn-success btn-rounded" type="submit">Guardar</button>
-                                </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <!-- Eliminar -->
-            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalEliminar">
-                eliminar
-            </button>
-
-
-            <div class="modal fade" id="modalEliminar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Eliminar elemento</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <form action="#" method="post" onsubmit="return validaE(this);">
-                                <div class="row">
-                                    <div class="col-lg-6 col-md-6 md-form">
-                                        <input type="text" name="Enombre" id="Ename" class="text-uppercase form-control">
-                                        <label for="Ename">Nombre</label>
-                                    </div>
-                                </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                            <button type="submit" class="btn btn-primary">Guardar cambios</button>
-                        </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-
-
-            <!-- Tabla de visualizacion de datos -->
-
-            <div class="card">
-                <div class="card-body">
-                    <table id="account-grid" class="table table-responsive table-hover">
-                        <thead>
-                            <th>#</th>
-                            <th>Nombre</th>
-                            <th>Precio</th>
-                            <th>Cantidad</th>
-                            <th>Descripcion</th>
-                            <th>Categoria</th>
-                            <th>Fecha de ingreso</th>
-                        </thead>
-                        <tbody id="account-grid-body"></tbody>
-                    </table>
-                    <div id="grid-alert" class="alert alert-warning" role="alert">
-                        <i class="fas fa-info-circle"></i><strong id="grid-message"></strong>
-                    </div>
-                </div>
-            </div>
-
-            <div class="modal fade" id="modalDetalle" tabindex="-1" role="dialog" aria-labelledby="modelTitledIs" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header bg-primary">
-                            <h5 class="modal tittle text-white font-weight-bold">Detalle del producto</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="container-fluid">
-                                <div class="row">
-                                    <div class="col-lg-4 col-sm-4 text-primary font-weight-bold">Nombre</div>
-                                    <label id="nombre-agregar" class="col-6">[nombre]</label>
-                                </div>
-                                <div class="row">
-                                    <div class="col-lg-4 col-sm-4 text-primary font-weight-bold">Precio</div>
-                                    <label for="precio-agregar" class="col-6">[precio]</label>
-                                </div>
-                                <div class="row">
-                                    <div class="col-lg-4 col-sm-4 text-primary font-weight-bold">Cantidad</div>
-                                    <label for="cantidad-agregar" class="col-6">[cantidad]</label>
-                                </div>
-                                <div class="row">
-                                    <div class="col-lg-4 col-sm-4 text-primary font-weight-bold">Descripcion de producto
-                                    </div>
-                                    <label for="descripcion-agregar" class="col-6">[descripcion]</label>
-                                </div>
-                                <div class="row">
-                                    <div class="col-lg-4 col-sm-4 text-primary font-weight-bold">categoria</div>
-                                    <label for="categoria-agregar" class="col-6">[categoria]</label>
-                                </div>
-                                <div class="row">
-                                    <div class="col-lg-4 col-sm-4 text-primary font-weight-bold">Fecha de igreso</div>
-                                    <label for="fecha-agregar" class="col-6">[fecha]</label>
-                                </div>
-
+                                <input id="nombre" name="nombre" type="text" class="form-control">
+                                <label for="nombre">Nombre</label>
                             </div>
+                            <div class="row">
+                                <textarea id="descripcion" name="descripcion" class="form-control"></textarea>
+                                <label for="descripcion">Descripcion<label>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        <button class="btn btn-success btn-rounded" type="submit">Guardar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="modalEliminar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Eliminar elemento</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="#" method="post" onsubmit="return validaE(this);">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 md-form">
+                                    <input type="text" name="Enombre" id="Ename" class="text-uppercase form-control">
+                                    <label for="Ename">Nombre</label>
+                                </div>
+                            </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        <button type="submit" class="btn btn-primary">Guardar cambios</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <!-- Modal detalle -->
+        <div class="modal fade" id="modalDetalle" tabindex="-1" role="dialog" aria-labelledby="modelTitledIs" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header bg-primary">
+                        <h5 class="modal tittle text-white font-weight-bold">Detalle del producto</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-lg-4 col-sm-4 text-primary font-weight-bold">Nombre</div>
+                                <label id="nombre-agregar" class="col-6">[nombre]</label>
+                            </div>
+                            <div class="col-lg-4 col-sm-4 text-primary font-weight-bold">Descripcion
+                            </div>
+                            <label for="descripcion-agregar" class="col-6">[descripcion]</label>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-4 col-sm-4 text-primary font-weight-bold">Fecha de igreso</div>
+                            <label for="fecha-agregar" class="col-6">[fecha]</label>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     <?php include '../includes/scripts.php' ?>
+    <script src="../js/categorias.js"></script>
 </body>
 
 </html>

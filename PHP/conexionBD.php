b6eb2f81fe06fa408bdffce6fbdd1c8e06336b33
Isai Polito <ispolitoc@gmail.com>
2022-06-10 19:14:03 -0500

    fix: connection db php

--- PHP/conexionBD.php
+++ PHP/conexionBD.php
@@ -1,32 +1,16 @@
 <?php
-    $servidor = "localhost";
-    $usuario = "root";
-    $contra = "";
-    $bd = "aldoups_proyecto";
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "aldoups_proyecto";
 
-    $conexion = new mysqli($servidor,$usuario,$contra,$bd);
+try {
+    /** Se realiza la conexión */
+    $conn = new PDO("mysql:host=$hostname;dbname=$database;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    if(isset($_POST["nombre"])){
-        $nombre = $_POST ["nombre"];
-        $precio = $_POST ["precio"];
-        $cantidad = $_POST ["cantidad"];
-        $descripcion = $_POST ["descripcion"];
-        $categoria = $_POST["categoria"];
-        $fecha_ingreso = $_POST ["fecha"];
+    echo "connection successfully\n"; 
 
-        $sql = "INSERT INTO agregar(nombre,precio,cantidad,descripcion,categoria,fecha) values('$nombre','$precio','$cantidad','$descripcion','$categoria','$fecha_ingreso')";
-        mysqli_query($conexion,$sql);    
-    }
-
-    try{
-        $sql == $conn->prepare('SELECT * FROM agregar');
-        $sql ->execute();
-
-        $result = $sql->fetchAll();
-
-        echo json_encode($result);
-
-    }catch (PDOException $e) {
-        die('ERROR'. $e->getMessage());
-    }
-?>
+} catch (PDOException $e) {
+    die('ERROR' . $e->getMessage());
+}

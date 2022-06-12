d689fe01112894e466afa642b0c0a6da597c2cba
Isai Polito <ispolitoc@gmail.com>
2022-06-12 02:06:26 -0500

    add constants to common, export theses

--- js/common.js
+++ js/common.js
@@ -1,1 +1,5 @@
 const ROOT_URL = 'http://ups.test'
+const categorias_url = `${ROOT_URL}/categorias`
+const productos_url = `${ROOT_URL}/productos`
+
+export { categorias_url, productos_url, ROOT_URL }

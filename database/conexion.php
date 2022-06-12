01c88c694ea9ce2e5f17121ae64a13d899c9e149
Isai Polito <ispolitoc@gmail.com>
2022-06-12 00:06:24 -0500

    update db connection php

--- database/conexion.php
+++ database/conexion.php
@@ -8,9 +8,6 @@ try {
     /** Se realiza la conexión */
     $conn = new PDO("mysql:host=$hostname;dbname=$database;charset=utf8", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    echo "connection successfully\n"; 
-
 } catch (PDOException $e) {
     die('ERROR' . $e->getMessage());
 }

3437aadc350a827e87a75e0e8f510b69655940bb
Isai Polito <ispolitoc@gmail.com>
2022-06-14 03:44:45 -0500

    move popper js path

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,2 +1,2 @@
-<script type="text/javascript" src="/js/popper.min.js"></script>
+<script type="text/javascript" src="/js/popper/popper.min.js"></script>
 <script type="text/javascript" src="/js/bootstrap/bootstrap.min.js"></script>

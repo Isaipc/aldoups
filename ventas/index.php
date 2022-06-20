5a6c37a8a791f231c6c3a61e1d24d780f023d9e2
Isai Polito <ispolitoc@gmail.com>
2022-06-20 09:35:37 -0500

    update ventas index title

--- ventas/index.php
+++ ventas/index.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ventas</title>
+    <title>Historial de ventas</title>
     <?php include '../includes/head.php' ?>
 </head>
 
 <body>
     <?php include '../includes/navbar.php' ?>
     <div class="container p-2">
-        <h2>Ventas</h2>
+        <h2>Historial de ventas</h2>
         <!-- Tabla de visualizacion de datos -->
         <div class="card">
             <div class="card-body">

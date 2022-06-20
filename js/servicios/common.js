3eef606adff2f0f1550d1d219960bbe680657969
Isai Polito <ispolitoc@gmail.com>
2022-06-20 08:50:48 -0500

    add ventas urls contants

--- js/servicios/common.js
+++ js/servicios/common.js
@@ -1,5 +1,6 @@
 const ROOT_URL = 'http://ups.test'
 const categorias_url = `${ROOT_URL}/categorias`
 const productos_url = `${ROOT_URL}/productos`
+const ventas_url = `${ROOT_URL}/ventas`
 
-export { categorias_url, productos_url, ROOT_URL }
+export { categorias_url, productos_url, ventas_url, ROOT_URL }

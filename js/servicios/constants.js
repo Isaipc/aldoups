2821bee8bcfce5a08bcda8ace5bbe548e338251a
Isai Polito <ispolitoc@gmail.com>
2022-06-22 00:01:41 -0500

    add dt language options to constants

--- js/servicios/constants.js
+++ js/servicios/constants.js
@@ -5,11 +5,30 @@ const ventas_url = `${ROOT_URL}/ventas`
 const cortes_de_caja_url = `${ROOT_URL}/cortes-de-caja`
 const reportes_url = `${ROOT_URL}/reportes`
 
+const dt_language_options = {
+    emptyTable: "No hay datos disponibles",
+    zeroRecords: "No se encontraron resultados",
+    infoFiltered: "(filtrado de _MAX_ registros totales)",
+    infoEmpty: "Mostrando 0 registros",
+    search: 'Buscar',
+    info: "Mostrando pagina _PAGE_ de _PAGES_",
+    paginate: {
+        first: "Primero",
+        last: "Ultimo",
+        next: "Siguiente",
+        previous: "Anterior"
+    },
+    lengthMenu: "Mostrar _MENU_ filas",
+    processing: 'Procesando ...'
+};
+
+
 export {
     categorias_url,
     productos_url,
     ventas_url,
     cortes_de_caja_url,
     reportes_url,
-    ROOT_URL
+    ROOT_URL,
+    dt_language_options
 }

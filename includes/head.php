45e29101e2dd4eb87001c9c052c6b722c3a0542d
Isai Polito <ispolitoc@gmail.com>
2022-06-22 00:02:47 -0500

    add datatables library css/js

--- includes/head.php
+++ includes/head.php
@@ -1,3 +1,4 @@
     <link rel="stylesheet" href="/css/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="/css/bootstrap-icons/bootstrap-icons.css">
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.12.1/datatables.min.css"/>
--- includes/scripts.php
+++ includes/scripts.php
@@ -1,2 +1,3 @@
 <script type="text/javascript" src="/js/popper/popper.min.js"></script>
 <script type="text/javascript" src="/js/bootstrap/bootstrap.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.12.1/datatables.min.js"></script>
